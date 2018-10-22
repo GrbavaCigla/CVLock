2a28ccb378b65041866063780182ab01d9148a46
proalexa <alexa.ognjanovic@gmail.com>
2018-10-22 12:40:40 +0200

    Resolving Issues 2

--- checkdynamic.py
+++ checkdynamic.py
@@ -7,7 +7,7 @@ import sys;
 mainfolder="/usr/local/CVLock/"
 picfolder=join(mainfolder, sys.argv[1])
 threshold = 0.7
-tickness=2
+
 
 face_cascade = cv2.CascadeClassifier("/usr/local/CVLock/haarcascade_frontalface_default.xml")
 files = [join(picfolder,f) for f in listdir(picfolder) if isfile(join(picfolder, f))]
@@ -19,13 +19,15 @@ templates=[cv2.cvtColor(cv2.imread(i), cv2.COLOR_BGR2GRAY) for i in files]
 while 1:
         ret, img = cap.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        print("----------------------------Convert----------------------------")
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for template in templates:
-
+        for bh,template in enumerate(templates):
+                
+                print("template:{}".format(bh))
                 for (x,y,w,h) in faces:
                         res = cv2.matchTemplate(gray[y:y+h,x:x+w],template,cv2.TM_CCOEFF_NORMED)
                         loc = np.where( res >= threshold)
-
+                        print("----------------------------Face Found----------------------------")
 
                         for pt in zip(*loc[::-1]):
                                 #LOCK PART
@@ -33,6 +35,9 @@ while 1:
                                 quit(0)
                                 break
                                 #LOCK PART
+                                
+                                
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
                 quit(54)
 cap.release()
