edbe0db08bfba509c8e5704ffeb46ee3100b10ee
hwrnr <hawerner.hawerner@gmail.com>
2018-11-22 20:29:33 +0100

    Fixed permission error

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 mkdir /usr/local/CVLock 2> /dev/null;
 cp checkdynamic.py /usr/local/CVLock/;
+chmod +x /usr/local/CVLock/checkdynamic.py 
 cp haarcascade_frontalface_default.xml /usr/local/CVLock/;
 cd pam_module;
 ./install.sh
