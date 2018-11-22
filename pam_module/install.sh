fde62a67ed49a2bc8fe4664455d69c28f2881e1a
hwrnr <hawerner.hawerner@gmail.com>
2018-11-22 20:33:59 +0100

    .

--- pam_module/install.sh
+++ pam_module/install.sh
@@ -2,5 +2,7 @@ gcc -fPIC -fno-stack-protector -c main.c
 ld -x --shared -o /lib/security/pam_CVLock.so main.o 2>/dev/null
 ld -x --shared -o /lib64/security/pam_CVLock.so main.o 2>/dev/null
 ld -x --shared -o /lib/x86_64-linux-gnu/security/pam_CVLock.so main.o 2>/dev/null
-echo 'ps aux | grep checkdynamic.py | awk '{print $2}' | xargs kill; exit 0' >> /etc/gdm/PostLogin/Default
-chmod +x /etc/gdm/PostLogin/Default
+echo 'ps aux | grep checkdynamic.py | awk '{print $2}' | xargs kill; exit 0' >> /etc/gdm/PostLogin/Default 2> /dev/null
+chmod +x /etc/gdm/PostLogin/Default 2> /dev/null
+echo 'ps aux | grep checkdynamic.py | awk '{print $2}' | xargs kill; exit 0' >> /etc/gdm3/PostLogin/Default 2> /dev/null
+chmod +x /etc/gdm3/PostLogin/Default 2> /dev/null
