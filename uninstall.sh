73ec4a1640e2e9b09107a51aec5b5a6445ae1868
hwrnr <hawerner.hawerner@gmail.com>
2019-02-16 20:06:12 +0100

    Remove gdm-fingerprint

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,5 @@
 rm -rf /usr/local/CVLock;
 rm -f /lib/security/CVLock.so
+mv /etc/pam.d/gdm-fingerprint.old /etc/pam.d/gdm-fingerprint 2> /dev/null
+sudo -u gdm gsettings set org.gnome.login-screen enable-fingerprint-authentication false
+
